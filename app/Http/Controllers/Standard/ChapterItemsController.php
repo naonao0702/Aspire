6dc26a0ce67a39d521d92e4a52cbb77f1b42855b
naotarian <naonao.070294@gmail.com>
2020-05-30 21:38:15 +0900

    論理フラグ修正

--- app/Http/Controllers/Standard/ChapterItemsController.php
+++ app/Http/Controllers/Standard/ChapterItemsController.php
@@ -5,17 +5,22 @@ namespace App\Http\Controllers\standard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\ChapterItem;
+use Illuminate\Support\Facades\Auth;
+use App\ClearItem;
+
 
 class ChapterItemsController extends Controller
 {
     public function index($language_id, $chapter_num)
     {
+        $login_user = \Auth::user();
+        $user_id = $login_user->id;
         $id = $language_id;
         $chapter = $chapter_num;
         $datas = ChapterItem::where('language_id', $id)->where('chapter_num', $chapter)
             ->OrderBy('chapter_item_num', 'asc')
             ->get();
-        $complete = count(ChapterItem::where('language_id', $id)->where('chapter_num', $chapter)->where('is_complete', 1)->get());
+        $complete = count(ClearItem::where('user_id', $user_id)->where('language_id', $id)->where('chapter_num', $chapter)->get());
 
         $all = count(ChapterItem::where('language_id', $id)->where('chapter_num', $chapter)->get());
 
--- app/Http/Controllers/Standard/CompleteController.php
+++ app/Http/Controllers/Standard/CompleteController.php
@@ -5,22 +5,53 @@ namespace App\Http\Controllers\standard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\ChapterItem;
+use App\ClearItem;
+use Illuminate\Support\Facades\Auth;
 
 class CompleteController extends Controller
 {
     public function complete($item_id)
     {
         $data = ChapterItem::find($item_id);
-        if ($data->is_complete == 1) {
-            $data->is_complete = 0;
-        } elseif ($data->is_complete == 0) {
-            $data->is_complete = 1;
-        }
-        $data->save();
+        $user_data = \Auth::user();
+
+        $clear_data = new ClearItem();
+        $clear_data->language_id = $data->language_id;
+        $clear_data->chapter_num = $data->chapter_num;
+        $clear_data->chapter_item_num = $data->chapter_item_num;
+        $clear_data->user_id = $user_data->id;
+        $clear_data->user_name = $user_data->name;
+
+        $clear_data->save();
+
 
         return redirect(route('lesson-item', [
             'language_id' => $data->language_id,
             'chapter_num' => $data->chapter_num,
+            // 'user_id' => $user_data->id,
+        ]));
+    }
+
+    public function notcomplete($language_id, $chapter_num, $chapter_item_num)
+    {
+        $user_data = \Auth::user();
+        $clear_data = ClearItem::where('user_id', $user_data->id)
+            ->where('language_id', $language_id)
+            ->where('chapter_num', $chapter_num)
+            ->where('chapter_item_num', $chapter_item_num)
+            ->get();
+
+        $clear_id = $clear_data[0]['clear_id'];
+
+        $delete_data = ClearItem::find($clear_id);
+
+        // $chapter_data = ClearItem::find($item_id);
+        $delete_data->delete();
+
+        return redirect(route('lesson-item', [
+            'language_id' => $language_id,
+            'chapter_num' => $chapter_num,
+            // 'user_id' => $user_data->id,
         ]));
     }
 
