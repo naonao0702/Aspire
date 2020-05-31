6d4559e3d1b7c5376736d6fc1c589d9e3efb9c25
EC2 Default User <ec2-user@ip-172-31-33-171.ap-northeast-1.compute.internal>
2020-05-31 08:25:50 +0000

    画像表示 or css update

--- public/index.php
+++ public/index.php
@@ -50,11 +50,15 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 */
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
+// dd($kernel->handle);
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
 
+// dd($request);
+// dd($response);
+
 $response->send();
 
 $kernel->terminate($request, $response);
+
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,12 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--fontawesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <!--css-->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <!--bootstrap-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>@yield('title')</title>
 </head>
 
 <body>
-    <div class="container">
+    <div class="header-container">
         <div class="header-wrapper">
             <div class="header-box-wrapper">
                 <div class="header-content">
@@ -25,11 +30,15 @@
         </div>
 
         <div class="content-wrapper">
-            <div class="main-content">@yield('contents')</div>
-            <div class="side-bar-content">@yield('side-bar-contents')</div>
+            <div class="container">
+                <div class="main-content">
+                        @yield('contents')
+                </div>
+                <div class="footer">フッターです</div>
+            </div>
         </div>
 
-        <div class="footer">フッターです</div>
+        <!--<div class="footer">フッターです</div>-->
     </div>
 
 </body>
