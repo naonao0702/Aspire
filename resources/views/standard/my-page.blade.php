a46f978d2fb5fdf5b583d540eb0e68f6a786cd1a
KEN3pei <kensuke1202left@icloud.com>
2020-06-05 17:34:44 +0000

    topページの編集

--- resources/views/standard/my-page.blade.php
+++ resources/views/standard/my-page.blade.php
@@ -1,19 +1,22 @@
-@extends('layout.app')
+@extends('layout.top')
 
 
 
 @section('contents')
 
-<div class="head-user">
-    <div class="my-page-top">
-        <p>お帰りなさい {{ $user->name }} さん</p>
-    </div>
+<div class="top-aspire">
+    <!--<div class="my-page-top">-->
+    <!--    <p>お帰りなさい {{ $user->name }} さん</p>-->
+    <!--</div>-->
 
-    <div class="my-page">
-        <p>受講中カリキュラム</p>
-    </div>
+    <!--<div class="my-page">-->
+    <!--    <p>受講中カリキュラム</p>-->
+    <!--</div>-->
 
-    <div class="my-page-bottom"></div>
+    <!--<div class="my-page-bottom"></div>-->
+    <div class="header-bgimg">
+        <a href="/lesson-list" class="top-button small">レッスン一覧</a>
+    </div>
 </div>
 @endsection
 
